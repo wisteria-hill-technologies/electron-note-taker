abe6e1385f2e0adee12b6eebe4afc019ede592d9
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-11-06 00:37:52 +0000

    adds hashing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CryptoJS from 'crypto-js';
 
 class App extends Component {
 
   state={
-    privateKey: ""
+    privateKey: "",
+    password: ""
   }
 
   render() {
+    const mySecretKey = `${this.state.privateKey}-${this.state.password}`;
+    const hashedPassword = btoa(CryptoJS.PBKDF2(mySecretKey, this.state.privateKey).toString());
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Password Generator</h1>
-          <label>
-            Private key: <input onChange={(e) => this.setState({ privateKey: e.target.value})}/>
-          </label>
-
+          <div>
+            <p>
+              <label>
+                Private key: <input onChange={(e) => this.setState({ privateKey: e.target.value})}/>
+              </label>
+            </p>
+            <p>
+              <label>
+                Password: <input onChange={(e) => this.setState({ password: e.target.value})}/>
+              </label>
+            </p>
+            <p>Hashed Password:</p>
+            <p>{ hashedPassword }</p>
+          </div>
 
         </header>
       </div>
