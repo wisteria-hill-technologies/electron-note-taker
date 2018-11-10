6a96a078810a94259f84125a3dc62004f8cc95d9
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-11-10 02:39:58 +0000

    adds more buttons

--- src/App.js
+++ src/App.js
@@ -17,28 +17,40 @@ WebFont.load({
 class App extends Component {
 
   state={
-    readOnly: false,
-    value: ""
+    readOnly: true,
+    value: "",
+    selectedFile: null
   }
 
-  showSaveDialog =() => {
-    dialog.showSaveDialog((filename) => {
-      if(filename === undefined) {
-        console.log("Cancelled");
-        return;
-      }
-      fs.writeFile(filename, this.state.value, (err) => {
+  saveFile =() => {
+    if(this.state.selectedFile) {
+      fs.writeFile(this.state.selectedFile, this.state.value, (err) => {
         if(err) {
           console.log('error occured when creating the file.');
           return;
         }
-        alert('File successfully created.');
+        alert('File successfully saved.');
       });
-    });
+    } else {
+      dialog.showSaveDialog((filename) => {
+        if(filename === undefined) {
+          console.log("Cancelled");
+          return;
+        }
+        fs.writeFile(filename, this.state.value, (err) => {
+          if(err) {
+            console.log('error occured when creating the file.');
+            return;
+          }
+          alert('File successfully created.');
+        });
+      });
+    }
   }
 
-  showOpenDialog = () => {
+  openFile = () => {
     dialog.showOpenDialog((fileNames) => {
+      console.log('showOpenDialog fileNames >>> ', fileNames);
       if(fileNames === undefined) {
         console.log('No files were selected');
         return;
@@ -50,6 +62,8 @@ class App extends Component {
         }
         console.log('the content of the file is : ', data);
         this.setState({
+          readOnly: true,
+          selectedFile: fileNames[0],
           value: data
         });
         return;
@@ -68,6 +82,14 @@ class App extends Component {
     });
   }
 
+  closeFile = () => {
+    this.setState({
+      selectedFile: null,
+      value: "",
+      readOnly: true,
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -78,9 +100,27 @@ class App extends Component {
           handleTextChange={this.handleTextChange}
           readOnly={this.state.readOnly}
         />
-        <button onClick={this.toggleReadOnly}>{ this.state.readOnly ? 'Edit' : 'ReadOnly' }</button>
-        <button onClick={this.showSaveDialog}>Save your note to a file</button>
-        <button onClick={this.showOpenDialog}>Open a file</button>
+        {
+          this.state.value &&
+          <button onClick={this.toggleReadOnly}>{ this.state.readOnly ? 'Edit' : 'ReadOnly' }</button>
+        }
+        {
+          !this.state.readOnly &&
+          <button onClick={this.saveFile}>{this.state.selectedFile ? "Save" : "Save As" }</button>
+        }
+        {
+          !this.state.readOnly &&
+          <button onClick={this.closeFile}>Close file</button>
+        }
+        {
+          this.state.selectedFile && this.state.readOnly &&
+          <button onClick={this.closeFile}>Close file</button>
+        }
+        {
+          this.state.readOnly && !this.state.selectedFile &&
+          <button onClick={this.toggleReadOnly}>New</button>
+        }
+        <button onClick={this.openFile}>Open {this.state.selectedFile ? "another file" : "a file" }</button>
       </div>
     );
   }
